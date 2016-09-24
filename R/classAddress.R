973a3d32119e026946b50044b7d5a0d06486fbf8
Sascha Schmalz <sassc662@student.liu.se>
2016-09-24 14:52:37 +0200

    Changed name of class 'address' to 'adrs' because of conflicts and added Roxygen2 description:

--- R/classAddress.R
+++ R/classAddress.R
@@ -1,16 +1,34 @@
-address <- function(street, housenumber, state) {
+#' Lorem ipsulum
+#'
+#' Lorem ipsulum
+#'
+#'@param street A string
+#'@param housenumber A string
+#'@param state A string
+#'
+#'
+#'@export
+adrs <- function(street, housenumber, state) {
   'Function for creating objects of class address.'
+
+  # Check if the arguments have a valid type.
+  if(!any(sapply(c(street, housenumber, state), is.character))) {
+    stop('Test')
+  }
+
   structure(
     list(
       street = street,
       housenumber = housenumber,
       state = state
     ),
-    class = 'address'
+    class = 'adrs'
   )
 }
 
-print.address <- function(x, ...) {
+#'
+#'@export
+print.adrs <- function(x, ...) {
   cat('Address:\n')
   cat('---\n')
   cat(paste(paste(x$street, x$housenumber, sep = ' '), x$state, sep = ', '))
